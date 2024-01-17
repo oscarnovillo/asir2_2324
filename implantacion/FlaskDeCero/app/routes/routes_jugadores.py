975f081534fc2530069ee3b973defbb3629c20eb
oscarnovillo <oscar.novillo@gmail.com>
2024-01-17 09:16:08 +0100

    jjj

--- implantacion/FlaskDeCero/app/routes/routes_jugadores.py
+++ implantacion/FlaskDeCero/app/routes/routes_jugadores.py
@@ -3,6 +3,7 @@ from flask import Flask, Blueprint, render_template, request, redirect, url_for
 
 from app import db
 from app.data.jugadores_dao import JugadoresDao
+from app.data.equipos_dao import EquipoDao
 from app.data.modelo.equipo import Equipo
 
 
@@ -11,12 +12,8 @@ rutas_jugadores = Blueprint("routes_jugadores", __name__)
 
 @rutas_jugadores.route('/delJugador', methods=['POST'])   
 def delEquipo():
-    equipo_dao = EquipoDao()
-
     id = request.form['id']
-
-
-
+    equipo_dao = EquipoDao()
     equipo_dao.delete(db,id)
    
     return redirect(url_for('routes.verEquipos'))  
