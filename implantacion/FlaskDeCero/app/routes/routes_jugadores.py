e4fdc13dd1ad74a469e8b13f69bbb4034321aaea
oscarnovillo <oscar.novillo@gmail.com>
2024-01-23 10:17:13 +0100

    asdasd

--- implantacion/FlaskDeCero/app/routes/routes_jugadores.py
+++ implantacion/FlaskDeCero/app/routes/routes_jugadores.py
@@ -27,17 +27,18 @@ def verJugadores():
     equipo_dao = EquipoDao()
     equipos = equipo_dao.select_all(db)
     
+
     if (request.method == 'POST'):
         id_equipo = request.form['id_equipo']
         jugadores_dao = JugadoresDao()
         jugadores = jugadores_dao.select_all(db,id_equipo)
         for equipo in equipos:
-            if (equipo.id == id_equipo):
-                print(equipo)
+            
+            if (id_equipo == str(equipo.id)):
+               
                 nombreEquipo  = equipo.nombre
 
-    
-    print(nombreEquipo)
+
 
     return render_template('jugadores.html',
                            jugadores=jugadores,
