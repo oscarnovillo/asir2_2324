4ecd62ab3c80b01df470a83bf0b0fb0ac92e4fc0
oscarnovillo <oscar.novillo@gmail.com>
2024-01-31 10:52:57 +0100

    ssl

--- implantacion/FlaskDeCero/app/__init__.py
+++ implantacion/FlaskDeCero/app/__init__.py
@@ -7,6 +7,7 @@ import mysql.connector
 db = mysql.connector.connect( # LLAMAMOS AL FUNCION CONNECT PARA CONECTARNOS
     host ='informatica.iesquevedo.es',
     port = 3333,
+    ssl_disabled = True,
     user ='root', #USUARIO QUE USAMOS NOSOTROS
     password ='1asir', #CONTRASEÑA CON LA QUE NOS CONECTAMOS
     database='testFlask'
